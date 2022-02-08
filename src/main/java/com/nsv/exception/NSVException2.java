900da446c2f2ece17f9079a98098209421dcea0f
franmen05 <franmen05@gmail.com>
2022-02-07 22:24:45 -0400

    add exception

---
+++ src/main/java/com/nsv/exception/NSVException2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nsv.exception;
+
+/**
+ *
+ * @author you_k
+ */
+public class NSVException2 extends  RuntimeException{
+
+    public NSVException2(String msj) {
+        super(msj);
+    }
+    
+    
+}
